c8a6f63155f255950fc6133a373b599cc6d30d3b
Mykayla Fernandes <mykayla.fernandes@gmail.com>
2018-01-28 12:39:59 -0700

    added skilss section

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,7 +1,6 @@
-/*tabs functionality*/
-
-function tab_select() {
-    'use strict';
-    
-    
-}
+/*scroll to menu content*/
+$(".edu-btn").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".edu").offset().top},
+        'slow');
+});
